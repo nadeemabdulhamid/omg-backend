5ed28a20b801cce916651bdf5d67556903249efe
Nadeem Abdul Hamid <nadeem@acm.org>
2025-02-18 09:37:23 -0500

    stage 5 - cart functionality

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,25 @@ import omg.interfaces.*;
 import omg.server.OMGServer;
 
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         OMGServer server = new OMGServer("data/data-stage-4.json");
 
         server.installConstructor("audio", Audio.class, "id", "title", "description", "artist", "duration", "sale-price", "list-price", "discount", "tags", "rating-average", "rating-count");
         server.installConstructor("print", Book.class, "id", "title", "description", "author-name", "author-yob", "sale-price", "list-price", "discount", "tags", "rating-average", "rating-count");
         server.installConstructor("video", Movie.class, "id", "title", "description", "starring", "directed-by", "sale-price", "list-price", "discount", "tags", "rating-average", "rating-count");
+       
         ILoM items = server.fetchItemList(MTLoM::new, ConsLoM::new);
         Store store = new Store(items);
 
         server.installHandler("catalog",   (RequestCatalogHandler) store::catalog);
         server.installHandler("item-data", (RequestItemDataHandler) store::itemInfoAsJSON);
         server.installHandler("tags",      (RequestTagsWithLimitHandler) store::tagsList);
+
+        server.installHandler("cart-count", (RequestCatalogHandler) store::cartSize);
+        server.installHandler("cart-list",  (RequestCatalogHandler) store::cartList);
+        server.installHandler("cart-add",   (RequestItemDataHandler) store::addToCart);
+        server.installHandler("cart-remove", (RequestItemDataHandler) store::removeFromCart);
+
         server.start();
     }
 }
--- src/StoreTest.java
+++ src/StoreTest.java
@@ -7,8 +7,10 @@ import static org.junit.Assert.*;
 
 public class StoreTest extends MediaTest {
 
-    Store s1 = new Store(new ConsLoM(b1, new ConsLoM(a2, new ConsLoM(m1, new MTLoM()))));
-    Store s2 = new Store(new ConsLoM(m2, new ConsLoM(b3, new ConsLoM(a1, new MTLoM()))));
+    Store s1 = new Store(new ConsLoM(b1, new ConsLoM(a2, new ConsLoM(m1, new MTLoM()))));   // [1, 6, 7]
+    Store s2 = new Store(new ConsLoM(m2, new ConsLoM(b3, new ConsLoM(a1, new MTLoM()))));   // [8, 4, 3]
+    Store s3 = new Store(new ConsLoM(m2, new ConsLoM(b3, new ConsLoM(a1, new MTLoM()))), new ConsLoN(4, new MTLoN()));
+    Store s4 = new Store(new ConsLoM(m2, new ConsLoM(b3, new ConsLoM(a1, new MTLoM()))), new ConsLoN(8, new ConsLoN(4, new MTLoN())));
 
     @Test
     public void testCatalog() {    
@@ -29,6 +31,40 @@ public class StoreTest extends MediaTest {
             ["drama","classic","film","fiction","novella","france","rock","classic","album"]""", s2.tagsList(-1));
         assertEquals("""
             ["drama","classic","film","fiction","novella"]""", s2.tagsList(5));
+    }
+
+    @Test
+    public void testCartSize() {
+        assertEquals("0", s1.cartSize());
+        assertEquals("1", s3.cartSize());
+        assertEquals("2", s4.cartSize());
+    }
+
+    @Test 
+    public void testCartList() {
+        assertEquals("[]", s1.cartList());
+        assertEquals("[4]", s3.cartList());
+        assertEquals("[8,4]", s4.cartList());
+    }
+
+    @Test
+    public void testCartAddRemove() {
+        assertEquals("[]", s2.cartList());
+        assertEquals("true", s2.addToCart(4));
+        assertEquals(s3, s2);
+        assertEquals("false", s2.addToCart(4));
+        assertEquals(s3, s2);
+
+        assertEquals("true", s2.addToCart(8));
+        assertEquals(s4, s2);
+        assertEquals("false", s2.addToCart(4));
+        assertEquals("false", s2.addToCart(8));
+
+        assertEquals("false", s2.removeFromCart(3));
+        assertEquals(s4, s2);
+        assertEquals("true", s2.removeFromCart(8));
+        assertEquals("false", s2.removeFromCart(8));
+        assertEquals(s3, s2);
     }
 
 }
