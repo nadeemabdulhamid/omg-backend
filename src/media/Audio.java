fd9d12e7d7dff60628b23037a7e060630dff0455
Nadeem Abdul Hamid <nadeem@acm.org>
2025-04-30 20:19:20 -0400

    type moved to superclass

--- src/media/Audio.java
+++ src/media/Audio.java
@@ -48,7 +48,6 @@ public class Audio extends AbsItem {
     @Override
     public String toJSONString() {
         return super.toJSONObject()
-                .put("type", "audio")
                 .put("artist", this.artist)
                 .put("info-line", infoLine())
                 .toString();
