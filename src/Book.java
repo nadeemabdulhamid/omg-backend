5ae044ff6ebf8406f03a629d559325b2bb2d883a
Nadeem Abdul Hamid <nadeem@acm.org>
2025-09-17 09:30:12 -0400

    update some files

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Nadeem Abdul Hamid, 2025.
  */
 
@@ -7,89 +7,114 @@ import java.util.Objects;
 /** 
  * Represents a book in our online media store.
  */
-public class Book {
+public class Book implements IMedia {
     int id;
     String title;
-    String author;
-    int price;          // in cents
-    String kind;        // "fiction", "nonfiction", "textbook"
+    String description;
+    Author author;
+    Price price;
+    String kind;
+    Rating rating;
 
-    public Book(int id, String title, String author, int price, String kind) {
+    public Book(int id, String title, String description, Author author, Price price, String kind, Rating rating) {
         this.id = id;
         this.title = title;
+        this.description = description;
         this.author = author;
         this.price = price;
         this.kind = kind;
+        this.rating = rating;
+    }
+
+    // overloaded constructor
+    public Book(int id, String title, String description, String authorName, int authorYOB, int salePrice, int listPrice, String discount, String kind,
+            double ratingAverage, int ratingCount) {
+        this(id, title, description, new Author(authorName, authorYOB), new Price(salePrice, listPrice, discount), kind, new Rating(ratingAverage, ratingCount));
+    }
+
+    /**
+     * Return the ID of this book.
+     */
+    @Override
+    public int getId() {
+        return this.id;
+    }
+
+    /**
+     * Return the description of this book.
+     */
+    public String getDescription() {
+        return this.description;
+    }
+
+    /**
+     * Return a truncated version of the description of this book.
+     */
+    public String getShortDescription() {
+        return this.description.substring(0, Math.min(15, this.description.length())) + "...";
     }
 
     /**
      * Return true if this book was written by the given author.
      */
     public boolean writtenBy(String author) {
-        return this.author.equals(author);
+        return this.author.nameMatches(author);
     }
 
     /**
-     * Return true if this book is more expensive than the given price.
+     * Return true if this book's sale price is more expensive than the given price.
      */
     public boolean moreExpensiveThan(int price) {
-        return this.price > price;
+        return this.price.getSalePrice() > price;
     }
-
-    /**
-     * Produce the (possibly discounted) sale price for this book. 
-     * The sale price of the book depends on the following discounts:
-     * - 30% discount on fiction books
-     * - 20% discount on nonfiction books
-     * - textbooks sell at full price
-	 */
-	public int salePrice(){    
-		if (this.kind.equals("fiction")) {
-			return this.price - 3 * (this.price / 10);
-		} else  if (this.kind.equals("nonfiction")) {
-			return this.price - 2 * (this.price / 10);
-		} else {
-			return this.price;
-		}
-	}
 
     /**
      * Produce an updated version of this book with the 
      * price multiplied by the given percentage.
      */
     public Book adjustPrice(int percent) {
-        return new Book(this.id, this.title, this.author, this.price * percent / 100, this.kind);
+        return new Book(this.id, this.title, this.description, this.author, this.price.adjustPrice(percent), this.kind, this.rating);
+    }
+
+    /**
+     * Return the sale price of this book.
+     */
+    @Override
+    public int salePrice() {
+        return this.price.getSalePrice();
+    }
+
+    /** Produce true if this media item contains the given text in 
+        any of its textual fields */
+    @Override
+    public boolean contains(String text) {
+        String lowerText = text.toLowerCase();
+        return this.title.toLowerCase().contains(lowerText) || this.description.toLowerCase().contains(lowerText) || this.author.contains(lowerText) || this.kind.toLowerCase().contains(lowerText);
     }
 
     /**
      * Return a JSON string representation of this book.
      */
-    public String toJSONString() {
-        return "{ \"type\": \"print\", \"id\": " + this.id + ", \"title\": \"" + this.title
-                 + "\", \"author\": \"" + this.author + "\", \"price\": \"$" 
-                 + (this.price/100) + "." + (this.price%100) + "\", \"tags\": \"" + this.kind + "\" }";
-    }
-
-
-// In VS Code, right-click at the bottom of the class definition, select "Source Action", 
-// then "Generate toString()"...
-
     @Override
-    public String toString() {
-        return "Book [id=" + id + ", title=" + title + ", author=" + author + ", price=" + price + ", kind=" + kind
-                + "]";
+    public String toJSONString() {
+        return "{ "
+                + StringHelpers.keyValuePair("type", "print") + ", "
+                + StringHelpers.keyValuePair("id", getId()) + ", "
+                + StringHelpers.keyValuePair("title", this.title) + ", "
+                + StringHelpers.keyValuePair("description-full", this.description) + ", "
+                + StringHelpers.keyValuePair("description-short", this.getShortDescription()) + ", "
+                + StringHelpers.keyValuePair("author", this.author.toJSONString(), false) + ", "
+                + StringHelpers.keyValuePair("price", this.price.toJSONString(), false) + ", "
+                + StringHelpers.keyValuePair("tags", this.kind) + ", "
+                + this.rating.toJSONStringFragment()
+                + " }";
     }
 
-// In VS Code settings, set both `Java › Code Generation › Hash Code Equals: Use Instanceof`
-// and `Java › Code Generation › Hash Code Equals: Use Java 7 Objects` to true.
-// This will generate the following `hashCode` and `equals` methods.
-
-// In VS Code, right-click at the bottom of the class definition, select "Source Action", 
-// then "Generate hashCode() and equals()"...
+    // AUTO-GENERATED: DO NOT EDIT BELOW
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, author, price, kind);
+        return Objects.hash(id, title, description, author, price, kind);
     }
 
     @Override
@@ -99,8 +124,15 @@ public class Book {
         if (!(obj instanceof Book))
             return false;
         Book other = (Book) obj;
-        return id == other.id && Objects.equals(title, other.title) && Objects.equals(author, other.author)
-                && price == other.price && Objects.equals(kind, other.kind);
+        return id == other.id && Objects.equals(title, other.title) && Objects.equals(description, other.description)
+                && Objects.equals(author, other.author) && Objects.equals(price, other.price)
+                && Objects.equals(kind, other.kind);
+    }
+
+    @Override
+    public String toString() {
+        return "Book [id=" + id + ", title=" + title + ", description=" + description + ", author=" + author
+                + ", price=" + price + ", kind=" + kind + "]";
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
 import omg.interfaces.RequestCatalogHandler;
 import omg.interfaces.RequestItemDataHandler;
+import omg.interfaces.RequestTagsHandler;
 import omg.server.OMGServer;
 
-void main() {
-    OMGServer server = new OMGServer("data/data-stage-1.json");
+public class Main {
+        public static void main(String[] args) {
+        OMGServer server = new OMGServer("data/data-stage-3.json");
 
-    server.installConstructor("print", Book.class, "id", "title", "author", "price", "tag");    // Book class and constructor need to be `public`
-    Book book = server.fetchOneItemData();
+        server.installConstructor("audio", Audio.class, "id", "title", "description", "artist", "duration", "sale-price", "list-price", "discount", "tag", "rating-average", "rating-count");
+        server.installConstructor("print", Book.class, "id", "title", "description", "author-name", "author-yob", "sale-price", "list-price", "discount", "tag", "rating-average", "rating-count");
+        server.installConstructor("video", Movie.class, "id", "title", "description", "starring", "directed-by", "sale-price", "list-price", "discount", "tag", "rating-average", "rating-count");
+        IMedia m1 = (IMedia)server.fetchItemList().get(0);
+        IMedia m2 = (IMedia)server.fetchItemList().get(2);
+        IMedia m3 = (IMedia)server.fetchItemList().get(6);
+        Store store = new Store(m1, m2, m3);
 
-    server.installHandler("catalog", (RequestCatalogHandler)() -> "[ 1 ]");
-    server.installHandler("item-data", (RequestItemDataHandler)(_) -> book.toJSONString());
-    server.start();
+        server.installHandler("catalog",   (RequestCatalogHandler) store::catalog);
+        server.installHandler("item-data", (RequestItemDataHandler) store::itemInfoAsJSON);
+        server.installHandler("tags",      (RequestTagsHandler) store::tagCounts);
+        server.start();
+    }
 }
-
